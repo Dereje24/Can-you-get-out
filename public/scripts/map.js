bc787ab4b8138ff5a30bf248c95ec7430e08694f
xingzo <kingsleynyaosi@gmail.com>
2018-03-16 14:51:56 -0700

    added icon to the map

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,6 +1,9 @@
 
-
+//accessToken
 mapboxgl.accessToken = 'pk.eyJ1IjoieGluZ3pvIiwiYSI6ImNqZXU2cXV3cTAzczUyeG1tZG03Z25zbWgifQ.ej8aUunGo8OUsbRG3XGjuw';
+
+
+//declare our map
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/xingzo/cjeu6x4f908j22rpez3ersfua',
@@ -8,14 +11,48 @@ var map = new mapboxgl.Map({
     center: [-83.765, 34.974]
 });
 
-var layerList = document.getElementById('menu');
-var inputs = layerList.getElementsByTagName('input');
 
-function switchLayer(layer) {
-    var layerId = layer.target.id;
-    map.setStyle('mapbox://styles/xingzo/cjeu6x4f908j22rpez3ersfua' + layerId + '-v9');
-}
+// _________________________ _________________________
+//MAP MENU FUNCTIONS
+// _________________________ _________________________
+// var layerList = document.getElementById('menu');
+// var inputs = layerList.getElementsByTagName('input');
+//
+// function switchLayer(layer) {
+//     var layerId = layer.target.id;
+//     map.setStyle('mapbox://styles/xingzo/cjeu6x4f908j22rpez3ersfua' + layerId + '-v9');
+// }
+//
+// for (var i = 0; i < inputs.length; i++) {
+//     inputs[i].onclick = switchLayer;
+// }
 
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].onclick = switchLayer;
-}
+
+
+map.on('load', function() {
+    map.loadImage('http://localhost:3000/images/logo4.png', function(error, image) {
+        if (error) throw error;
+        map.addImage('cat', image);
+        map.addLayer({
+            "id": "points",
+            "type": "symbol",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [-83.765, 34.974]
+                        }
+                    }]
+                }
+            },
+            "layout": {
+                "icon-image": "cat",
+                "icon-size": 0.25
+            }
+        });
+    });
+});
