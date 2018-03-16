3c1abe4af55aa22669f7561dbeacf1783fc2a844
xingzo <kingsleynyaosi@gmail.com>
2018-03-16 14:57:50 -0700

    added cors to server.js

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true}));
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
+// Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 /**********
  * ROUTES *
 ***********/
