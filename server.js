cc5e695ad42327bc13856ddd470e88ccae1c6f05
eches <Elaine.Chesoni83@gmail.com>
2018-03-16 18:03:33 -0700

    added video to landing page

--- server.js
+++ server.js
@@ -19,9 +19,12 @@ app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/landing', function homepage(req, res) {
+  res.sendFile(__dirname + '/views/landing.html');
+});
+
 //JSON API ENDPOINTS
 
-<<<<<<< HEAD
 //QUIZ ENDPOINTS
 app.get('/api/quiz', controller.quizes.index);
 
@@ -54,17 +57,6 @@ app.get('/api/answers/:id', controller.answers.show);
 app.put('/api/answers/:id', controller.answers.update);
 
 app.delete('/api/answers/:id', controller.answers.destroy);
-=======
-app.get('/api/quiz', controller.quiz.index);
-
-app.post('/api/quiz', controller.quiz.create);
-
-app.get('/api/quiz/:id', controller.quiz.show);
-
-app.put('/api/quiz/:id', controller.quiz.update);
-
-app.delete('/api/quiz/:id', controller.quiz.destroy);
->>>>>>> 9fc18153fe97dbe1ca8737ae038454c78a98bc33
 
 /**********
  * SERVER *
