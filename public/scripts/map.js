24c1599aae8b4a80507eb86fef01336a1026fa02
xingzo <kingsleynyaosi@gmail.com>
2018-03-17 00:33:38 -0700

    added AJAX call to get all questions from our database

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+$.ajax({
+	method: 'GET',
+	url: 'http://localhost:3000/api/questions',
+	success: handleSuccess
+
+});
+
+
+
+
+
 
 //accessToken
 mapboxgl.accessToken = 'pk.eyJ1IjoieGluZ3pvIiwiYSI6ImNqZXU2cXV3cTAzczUyeG1tZG03Z25zbWgifQ.ej8aUunGo8OUsbRG3XGjuw';
@@ -27,32 +39,89 @@ var map = new mapboxgl.Map({
 //     inputs[i].onclick = switchLayer;
 // }
 
+// $loadMarkerswithQuestions =  function(allQuestions){
+//
+//   loadQuestion(allQuestions)
+//
+//
+// };
+
+$loadQuestion = function(allQuestions){
+  console.log("in the loaded markers function");
+
+  map.on('load', function() {
+      map.loadImage('http://localhost:3000/images/logo4.png', function(error, image) {
+          if (error) throw error;
+          map.addImage('cat', image);
+          map.addLayer({
+              "id": "points",
+              "type": "symbol",
+              "source": {
+                  "type": "geojson",
+                  "data": {
+                      "type": "FeatureCollection",
+                      "features": [{
+                          "title": "Tribe 1",
+                          "type": "Feature",
+                          "properties": {
+                            "description":
+                            `<strong>Question</strong><p> ${allQuestions.questions[0].question} <a href=\"http://www.muhsinah.com\" target=\"_blank\" title=\"Opens in a new window\">Muhsinah</a> plays the <a href=\"http://www.blackcatdc.com\">Black Cat</a> (1811 14th Street NW) tonight with <a href=\"http://www.exitclov.com\" target=\"_blank\" title=\"Opens in a new window\">Exit Clov</a> and <a href=\"http://godsilla.bandcamp.com\" target=\"_blank\" title=\"Opens in a new window\">Gods’illa</a>. 9:00 p.m. $12.</p>`,
+                            "icon": "music"
+                          },
+                          "geometry": {
+                              "type": "Point",
+                              "coordinates": [-83.765, 34.974]
+                          }
+                      }]
+                  }
+              },
+              "layout": {
+                  "text-field": "{title}",
+                  "icon-image": "cat",
+                  "icon-size": 0.25
+              }
+          });
+      });
+
+      // When a click event occurs on a feature in the places layer, open a popup at the
+  // location of the feature, with description HTML from its properties.
+  map.on('click', 'points', function (e) {
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var description = e.features[0].properties.description;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+  });
+
+  // Change the cursor to a pointer when the mouse is over the places layer.
+  map.on('mouseenter', 'places', function () {
+      map.getCanvas().style.cursor = 'pointer';
+  });
+
+  // Change it back to a pointer when it leaves.
+  map.on('mouseleave', 'places', function () {
+      map.getCanvas().style.cursor = '';
+  });
+  });
 
 
-map.on('load', function() {
-    map.loadImage('http://localhost:3000/images/logo4.png', function(error, image) {
-        if (error) throw error;
-        map.addImage('cat', image);
-        map.addLayer({
-            "id": "points",
-            "type": "symbol",
-            "source": {
-                "type": "geojson",
-                "data": {
-                    "type": "FeatureCollection",
-                    "features": [{
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [-83.765, 34.974]
-                        }
-                    }]
-                }
-            },
-            "layout": {
-                "icon-image": "cat",
-                "icon-size": 0.25
-            }
-        });
-    });
+};
+
+
+
+function handleSuccess(json) {
+  allProjects = json;
+  console.log(allProjects);
+  $loadQuestion(allProjects);
+}
+
 });
